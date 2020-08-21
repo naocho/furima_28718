647b92f0b5bee5fb028535092b22d93d5b1395b2
naocho <kirateiru831@gmail.com>
2020-08-21 19:44:35 +0900

    ジェムの追加

---
+++ app/controllers/furima_controller.rb
@@ -0,0 +1,31 @@
+class FurimaController < ApplicationController
+
+def index
+  
+end
+
+def new
+  
+end
+
+def create
+
+end
+
+def destroy
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+def show
+  
+end
+
+end
