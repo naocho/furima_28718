d3c133b8420183a4e784ec5ef7c1162fc1af0dcb
naocho <kirateiru831@gmail.com>
2020-08-23 15:43:07 +0900

    コントローラーの修正

--- app/controllers/furima_controller.rb
+++ app/controllers/furima_controller.rb
@@ -1,5 +1,4 @@
 class FurimaController < ApplicationController
-  before_action :basic_auth
 
 def index
   
@@ -29,12 +28,5 @@ def show
   
 end
 
-private
-
-def basic_auth
-  authenticate_or_request_with_http_basic do |username, password|
-    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
-  end
-end
 
 end
