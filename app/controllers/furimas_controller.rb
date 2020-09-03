8c7f9d78e35969a67f60f61729e06d135281054a
naocho <kirateiru831@gmail.com>
2020-09-03 10:58:46 +0900

    商品一覧表示機能２回目

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -4,7 +4,7 @@ class FurimasController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
 def index
-  @items = Furima.all
+  @items = Furima.all.order("created_at DESC")
 end
 
 def new
