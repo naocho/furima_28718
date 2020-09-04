a7beec95c33417baf2d5c670716ecbce0288240a
naocho <kirateiru831@gmail.com>
2020-09-04 16:17:52 +0900

    商品情報編集機能２回目

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -2,6 +2,7 @@
 # またコントローラとモデルは全て名前を統一させる事
 class FurimasController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :edit_update, only: [:edit, :update, :show]
 
 def index
   @items = Furima.all.order("created_at DESC")
@@ -35,16 +36,15 @@ end
 #購入画面のコントローラにて入れて、ログインしていないユーザーをサインインに飛ばす
 
 def edit
-  @item = Furima.find(params[:id])
+  # @item = Furima.find(params[:id])
 end
 #editとupdateはセットで考えて良いと思う
 #editで定義したのはupdateでも定義して使うことが可能
 def update
-  @item = Furima.find(params[:id])
+  # @item = Furima.find(params[:id])
   @item.update(item_params)
 
-  if @item.valid?
-    @item.save
+  if @item.update(item_params)
     redirect_to root_path
   else
     render :edit
@@ -52,7 +52,7 @@ def update
 end
 
 def show
-  @item = Furima.find(params[:id])
+  # @item = Furima.find(params[:id])
   #findメソッドはモデル名.find(レコード:id)と言う書き方
 end
 
@@ -69,5 +69,10 @@ def move_to_index
       redirect_to action: :index
     end
 end
+
+def edit_update
+  @item = Furima.find(params[:id])
+end
+
 
 end
