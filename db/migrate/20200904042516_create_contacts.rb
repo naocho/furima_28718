95c4e90b241b931164682b81fb715a6a150b5b2d
naocho <kirateiru831@gmail.com>
2020-09-25 14:54:01 +0900

    ワーカーなどの設定

--- db/migrate/20200904042516_create_contacts.rb
+++ db/migrate/20200904042516_create_contacts.rb
@@ -1,8 +1,8 @@
 class CreateContacts < ActiveRecord::Migration[6.0]
   def change
     create_table :contacts do |t|
-    t.bigint :user_id,                null: false, foreign_key: true
-    t.references :furima,                null: false, foreign_key: true
+    t.bigint :user_id,       null: false, foreign_key: true
+    t.references :furima,    null: false, foreign_key: true
     t.timestamps
     end
   end
